144c7bf0b578a9ac3195103ec5805592a82cb368
Amit Ranjan <amit@gmail.com>
2019-07-24 12:41:07 +0530

    test case

--- src/test/java/com/amdocs/amdocshellorestapi/AmdocsHellorestApiApplicationTests.java
+++ src/test/java/com/amdocs/amdocshellorestapi/AmdocsHellorestApiApplicationTests.java
@@ -1,6 +1,7 @@
 package com.amdocs.amdocshellorestapi;
 
 import org.junit.Test;
+import static org.junit.Assert.*;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -11,6 +12,7 @@ public class AmdocsHellorestApiApplicationTests {
 
 	@Test
 	public void contextLoads() {
+		assertTrue(true);
 	}
 
 }
